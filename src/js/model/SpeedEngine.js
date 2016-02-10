433249230a18ee06cfe18fd5cb1d660d8fb7f529
yoni <yamishav@cisco.com>
2016-02-10 16:04:24 +0200

    update all

---
+++ src/js/model/SpeedEngine.js
@@ -0,0 +1,23 @@
+SpeedReaderEngine = function () {
+    var test,chunkSize,WPM ;
+    var textArray = []; //split the text into words
+
+
+    /*API for the engine*/
+    return {
+
+        setText: function () {
+
+        },
+        clearText: function(){
+        },
+        setChunkSize: function(){
+
+        },
+        getNextChunk: function(){
+
+        },
+        restart: function() {},
+        setWPM : function(){}
+    }
+    }
