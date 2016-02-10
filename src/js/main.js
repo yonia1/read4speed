e21af89f8539809008f7ef75b8f8224d73275721
yoni <yamishav@cisco.com>
2016-02-10 16:54:29 +0200

    create engine for speed reading and create mock

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,23 @@
+spe = new SpeedReaderEngine();
 
+
+spe.setChunkSize(5);
+var start = spe.getNextChunk();
+console.log('start with ' + start);
+
+var display = function () {
+    setTimeout(function () {
+        if(!start) return;
+        document.getElementById('displayText').textContent = start;
+        start = spe.getNextChunk();
+        start && display();
+    }, 500);
+};
+var textChanged = function(){
+    console.log('text changed');
+    var text = document.getElementById('textPaste').value;
+
+    spe.setText(text);
+    spe.setChunkSize(2);
+    display();
+}
--- src/js/model/SpeedEngine.js
+++ src/js/model/SpeedEngine.js
@@ -1,23 +1,62 @@
 SpeedReaderEngine = function () {
-    var test,chunkSize,WPM ;
+    var text, chunkSize = 2, WPM;
     var textArray = []; //split the text into words
+    var wIdx = 0; // Index for the current chunk of words that is being displayed
 
+    /*Private functions*/
+    var formatString = function(str) {
+        str = str.replace(/ +(?= )/g,''); // Format the string to only one space
+        return str;
+    }
+    var setText = function (text) {
+
+        if (!text) throw "No text was provided ";
+        text = formatString(text);
+        textArray = text.split(" ");// split the text by white space
+        console.log(textArray);
+        wIdx = 0; //restart reading
+
+    };
+    var isInt =function (value) {
+        var x;
+        return isNaN(value) ? !1 : (x = parseFloat(value), (0 | x) === x);
+    }
+    var setChunk = function (size) {
+        if(!size || size <1) throw "Illegal size for chunk";
+        if(!isInt(size)) throw "Illegal size for chunk";
+
+        chunkSize = size;
+    }
+    var getNextChunk = function () {
+
+        if(!textArray) throw 'no text was provided';
+        // move to next
+        if(wIdx > textArray.length) return ; // finish reading
+        //return the next chunk size of words
+        var str = '' ;
+        var chunk = chunkSize+1;
+        while(--chunk) { // build the word
+
+            str += textArray[++wIdx]+" ";
+            if(wIdx > textArray.length) break ;
+        }
+        return str;  // the current chunk of requested words
+    }
+    var restartReading = function () {
+        wIdx = 0;
+
+    }
 
     /*API for the engine*/
     return {
 
-        setText: function () {
+        setText: setText,
+        clearText: function () {
+        },
+        setChunkSize: setChunk,
 
-        },
-        clearText: function(){
-        },
-        setChunkSize: function(){
+        getNextChunk: getNextChunk,
+        restart: restartReading
 
-        },
-        getNextChunk: function(){
-
-        },
-        restart: function() {},
-        setWPM : function(){}
     }
-    }
+}
